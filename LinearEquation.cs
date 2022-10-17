20e21fe11e69d330747807cdc9326236c7871671
Алексей <lebedevalexeyo@gmail.com>
2022-10-18 01:19:18 +1100

    Добавьте файлы проекта.

---
+++ LinearEquation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    struct LinearEquation
+    {
+        double k;
+        double b;
+        public LinearEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public double Solve()
+        {
+            return - b/k;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите коэффициенты k и b");
+                double k = Convert.ToDouble(Console.ReadLine());
+                if (k == 0)
+                {
+                    Console.WriteLine("Коэффициент k должен отличаться от нуля");
+                }
+                else
+                {
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    LinearEquation le = new LinearEquation(k, b);
+                    Console.WriteLine("X = {0}", le.Solve());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
